92688a9a9bf03fd3077369ca7037eb6e23c45c4e
higormonteiro <higor.tecinfor@gmail.com>
2020-03-13 15:55:38 -0300

    create app doctors

---
+++ doctors/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import Specialty, Doctor
+
+
+class DoctorAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'crm', 'phone']
+    list_display = ['name', 'crm', 'phone']
+    
+
+class SpecialtyAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_display = ['name']
+    
+
+admin.site.register(Doctor, DoctorAdmin)
+admin.site.register(Specialty, SpecialtyAdmin)
---
+++ doctors/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+class Specialty(models.Model):
+    name = models.CharField(max_length=90)
+
+    class Meta:
+        verbose_name = 'Specialty'
+        verbose_name_plural = 'Specialties'
+        
+    def __str__(self):
+        return self.name
+
+
+class Doctor(models.Model):
+    name = models.CharField(max_length=90)
+    crm = models.CharField(max_length=7, unique=True)
+    email = models.EmailField(null=True, blank=True)
+    phone = models.CharField(max_length=20, null=True, blank=True)
+    specialty = models.ForeignKey(
+        Specialty, on_delete=models.CASCADE
+    )
+
+    class Meta:
+        verbose_name = 'Doctor'
+        verbose_name_plural = 'Doctors'
+        
+    def __str__(self):
+        return self.name + '-' + self.crm
