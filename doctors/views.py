502e4668d29185033622e4c45604099e7e508a2c
higormonteiro <higor.tecinfor@gmail.com>
2020-03-19 14:14:00 -0300

    organization the code

--- doctors/views.py
+++ doctors/views.py
@@ -1,4 +1,4 @@
-import django_filters.rest_framework
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics, filters
 
 from .models import Specialty, Doctor
@@ -15,8 +15,7 @@ class SpecialtyList(generics.ListAPIView):
 class DoctorList(generics.ListAPIView):
     queryset = Doctor.objects.all()
     serializer_class = DoctorSerializer
-    # filter_backends = [filters.SearchFilter]
-    # search_fields = ['name']
-    filter_backends = (filters.SearchFilter, django_filters.rest_framework.DjangoFilterBackend)
-    search_fields = ('name',)
-    filter_fields = ('specialty',)
+    filter_backends = [filters.SearchFilter,
+                       DjangoFilterBackend]
+    search_fields = ['name']
+    filter_fields = ['specialty', 'specialty__id']
